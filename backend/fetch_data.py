28fad8e672ec019e991483a69f05a88e07857e79
RidaDurani <rida.r.durani@gmail.com>
2025-04-13 00:07:49 +0100

    fetching_the_data_in_json_using_REST_API

---
+++ backend/fetch_data.py
@@ -0,0 +1,19 @@
+#Importing necessary libraries
+import requests
+import json
+import os
+import pandas as pd
+
+#API endpoint (URL) for Firestore to fetch data
+endpoint_url = "https://firestore.googleapis.com/v1/projects/rn-firebase-ml-test/databases/(default)/documents/patientData"
+
+#Output path to store the fetched data
+json_output_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'patient_data.json')
+csv_output_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'patient_data.csv')
+
+#Fetching data from Firebase
+response = requests.get(endpoint_url)
+data = response.json()
+#Saving the data to a JSON file
+with open(json_output_path, 'w') as f:
+    json.dump(data, f, indent=2)
